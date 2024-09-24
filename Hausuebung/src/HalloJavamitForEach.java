44efc70d640eb36d3fe0542a9c1af97015c38058
Jonas Doppelbauer <doppelbauerjonas@gmail.com>
2024-09-24 14:34:14 +0200

    2. Beispiel fertig

---
+++ Hausuebung/src/HalloJavamitForEach.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HalloJavamitForEach
+{
+
+    List<String> list = Arrays.asList("Hallo", "wie", "gehts");
+ 
+    public void showlistnormal ()
+    {
+        for (String s : list)
+        {
+            System.out.println(s);
+        }
+    }
+    public void showlistlambda()
+    {
+        System.out.println("1");
+        list.forEach(item -> System.out.println(item));
+        System.out.println("2");
+        list.forEach(System.out::println);
+
+    }
+
+
+
+
+}
