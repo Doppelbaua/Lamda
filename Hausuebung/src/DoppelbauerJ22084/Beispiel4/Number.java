97486292e03944c1b0f6aedb46f28c2140812b77
Jonas Doppelbauer <doppelbauerjonas@gmail.com>
2024-09-29 17:33:41 +0200

    class Number implimeted

--- Hausuebung/src/DoppelbauerJ22084/Beispiel4/Number.java
+++ Hausuebung/src/DoppelbauerJ22084/Beispiel4/Number.java
@@ -1,4 +1,24 @@
 package DoppelbauerJ22084.Beispiel4;
 
-public class Number {
+public class Number
+{
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
 }
+
